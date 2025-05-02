2835bc1e426efad9404ed127d255a3bb16ad7e2b
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-05-03 02:52:33 +0300

    Add tests for FindSegmentation, BaseSegmentationCalc, and DoubleSegmentation classes
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tab_right/base_architecture/seg_protocols_check.py
+++ tab_right/base_architecture/seg_protocols_check.py
@@ -1,9 +1,9 @@
-import pytest
 import abc
-import pandas as pd
+
+import pytest
 from sklearn.tree import DecisionTreeClassifier
 
-from .seg_protocols import FindSegmentation, BaseSegmentationCalc, DoubleSegmentation
+from .seg_protocols import DoubleSegmentation, FindSegmentation
 
 
 class TestFindSegmentation:
@@ -11,51 +11,46 @@ class TestFindSegmentation:
 
     @abc.abstractmethod
     @pytest.fixture
-    def fs(self) -> FindSegmentation:
+    def instance_to_check(self) -> FindSegmentation:
         """Fixture to create an instance of the class."""
 
-    def test_call(self, fs):
-
+    def test_call(self, instance_to_check):
         model = DecisionTreeClassifier()
-        result = fs('feature', lambda y, p: abs(y - p.mean(axis=1)), model)
-        assert 'segment_id' in result.columns
-        assert 'segment_name' in result.columns
-        assert 'score' in result.columns
+        result = instance_to_check("feature", lambda y, p: abs(y - p.mean(axis=1)), model)
+        assert "segment_id" in result.columns
+        assert "segment_name" in result.columns
+        assert "score" in result.columns
 
-    def test_protocol_followed(self, fs):
+    def test_protocol_followed(self, instance_to_check):
         """Test if the protocol is followed correctly."""
         # Check if the class has the required attributes
-        assert isinstance(fs, self.class_to_check)
+        assert isinstance(instance_to_check, self.class_to_check)
         # check if it is dataclass
-        assert hasattr(fs, '__dataclass_fields__')
+        assert hasattr(instance_to_check, "__dataclass_fields__")
 
 
 class TestBaseSegmentationCalc:
-
     @abc.abstractmethod
     @pytest.fixture
-    def bsc(self) -> BaseSegmentationCalc:
+    def instance_to_check(self) -> FindSegmentation:
         """Fixture to create an instance of the class."""
 
-    def test_call(self, bsc):
-
-        result = bsc( lambda y, p: abs(y - p).mean())
-        assert 'segment_id' in result.columns
-        assert 'score' in result.columns
+    def test_call(self, instance_to_check):
+        result = instance_to_check(lambda y, p: abs(y - p).mean())
+        assert "segment_id" in result.columns
+        assert "score" in result.columns
 
 
 class TestDoubleSegmentation:
-
     @abc.abstractmethod
     @pytest.fixture
-    def ds(self) -> DoubleSegmentation:
+    def instance_to_check(self) -> DoubleSegmentation:
         """Fixture to create an instance of the class."""
 
-    def test_call(self, ds):
-
+    def test_call(self, instance_to_check):
         model = DecisionTreeClassifier()
-        result = ds('feature1', 'feature2', lambda y, p: abs(y - p.mean(axis=1)), model)
-        assert 'segment_id' in result.columns
-        assert 'feature_1' in result.columns
-        assert 'feature_2' in result.columns
-        assert 'score' in result.columns
+        result = instance_to_check("feature1", "feature2", lambda y, p: abs(y - p.mean(axis=1)), model)
+        assert "segment_id" in result.columns
+        assert "feature_1" in result.columns
+        assert "feature_2" in result.columns
+        assert "score" in result.columns
