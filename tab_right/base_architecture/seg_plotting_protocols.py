db9a291f02b13ea2b689c7eaee60e8a3f849db1b
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-05-03 17:40:00 +0300

    Add protocol-based design for segmentation plotting
    Introduce a `DoubleSegmPlotting` protocol and refactor double segmentation plotting logic into a structured interface. Improve code clarity by defining the schema for heatmap DataFrames and encapsulating reusable methods like `get_heatmap_df` and `plotly_heatmap`. Additionally, remove outdated documentation to align with updated functionality.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tab_right/base_architecture/seg_plotting_protocols.py
+++ tab_right/base_architecture/seg_plotting_protocols.py
@@ -1,13 +1,18 @@
 """Module for defining plotting protocols."""
 
+from dataclasses import dataclass
+from typing import Protocol
+
 import pandas as pd
 from plotly.graph_objects import Figure
 
 
-def plot_double_segmentation(
-    df: pd.DataFrame,
-) -> Figure:
-    """Plot the double segmentation of a given DataFrame as a bar chart.
+@dataclass
+class DoubleSegmPlotting(Protocol):
+    """Class schema for double segmentation plotting.
+
+    This class is used to define the interface for plotting double segmentations.
+    It includes the DataFrames to be plotted and the kind of plot to be created.
 
     Parameters
     ----------
@@ -19,12 +24,34 @@ def plot_double_segmentation(
         - `feature_2`: (str) the range or category of the second feature.
         - `score`: (float) The calculated error metric for the segment.
 
-    Returns
-    -------
-    Figure
-        A heatmap showing each segment with its corresponding avg score, col of heatmap is feature_1, row is feature_2.
-
     """
+
+    df: pd.DataFrame
+    metric_name: str = "score"
+
+    def get_heatmap_df(self) -> pd.DataFrame:
+        """Get the DataFrame for the heatmap. from the double segmentation df.
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing the groups defined by the decision tree model.
+            columns: feature_1 ranges or categories
+            index: feature_2 ranges or categories
+            content: The calculated error metric for the segment.
+
+        """
+
+    def plotly_heatmap(self) -> Figure:
+        """Plot the double segmentation of a given DataFrame as a heatmap.
+
+        Returns
+        -------
+        Figure
+            A heatmap showing each segment with its corresponding avg score,
+            from get_heatmap_df() method.
+
+        """
 
 
 def plot_single_segmentation(
